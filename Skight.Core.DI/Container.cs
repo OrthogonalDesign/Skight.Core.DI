9095ab268ceb434557c31053f38336d54d8e7eff
Wang Hao <wanghao@skight.com>
2018-09-17 23:09:40 -0700

    Add solution and project

---
+++ Skight.Core.DI/Container.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Skight.Core.Container
+{
+    public class Container
+    {
+        private static IContainer underlying_resolver;
+
+        public static IContainer current
+        {
+            get { return underlying_resolver; }
+        }
+
+        public static void initialize_with(IContainer resolver)
+        {
+            underlying_resolver = resolver;
+        }
+
+        public static T get<T>()
+        {
+            return underlying_resolver.get_an<T>();
+        }
+    }
+}
---
+++ TestShell/MSpecSelfTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Machine.Specifications;
+
+namespace TestShell
+{
+    class MSpecSelfTest
+    {
+        private It should_run_testing = () => 1.ShouldEqual(1);
+    }
+}
