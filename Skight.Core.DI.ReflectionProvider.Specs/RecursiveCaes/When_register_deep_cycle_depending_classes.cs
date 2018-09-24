af26cde99723fc0b80a3a9b4cb6e0c4ad3530bca
Hao Wang <haowang@central1.com>
2018-09-24 14:56:40 -0700

    Add deep cycle depenency unit test

---
+++ Skight.Core.DI.ReflectionProvider.Specs/RecursiveCaes/When_register_deep_cycle_depending_classes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Machine.Specifications;
+
+namespace Skight.Core.DI.ReflectionProvider.Specs
+{
+    public class When_register_deep_cycle_depending_classes : SetupContainer
+    {
+        private Establish context = () =>
+        {
+            registration.self_register<ClassA>();
+            registration.self_register<ClassB>();
+            registration.self_register<ClassC>();
+        };
+
+        private Because of = () => exception = Catch.Exception(() =>container.get<ClassA>());
+
+        It should = () => exception.ShouldBeAssignableTo<ApplicationException>();
+               
+        private static Exception exception;
+
+        class ClassA
+        {
+            public ClassA(ClassB b)
+            {
+                
+            }
+        }
+
+        class ClassB
+        {
+            public ClassB(ClassC c)
+            {
+                          
+            }
+            
+        }
+
+        class ClassC
+        {
+            public ClassC(ClassA a)
+            {
+                
+            }
+        }
+    }
+}
